ad2e7f594a4a02edf044544bbb11eb5589efd7b3
Aaron Christiansen <aaronc20000@gmail.com>
2019-11-13 21:55:32 +0000

    Nevermind, Sorbet has #from_hash

--- spec/loc_spec.rb
+++ spec/loc_spec.rb
@@ -3,20 +3,21 @@
 RSpec.describe(SorbetCFG::Tree::Loc) do
   it 'can be created from a hash' do
     result = described_class.from_hash({
-      path: './foo.rb',
-      position: {
-        start: {
-          line: 8,
-          column: 3
+      'path' => './foo.rb',
+      'position' => {
+        'start' => {
+          'line' => 8,
+          'column' => 3
         },
-        end: {
-          line: 8,
-          column: 20
+        'end' => {
+          'line' => 8,
+          'column' => 20
         }
       }
     })
 
     expect(result.path).to eq './foo.rb'
+    expect(result.position).to be_a SorbetCFG::Tree::Loc::Position
     expect(result.position.start.column).to eq 3
   end
 end
